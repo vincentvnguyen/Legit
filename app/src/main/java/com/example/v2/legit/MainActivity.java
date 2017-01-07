0db27d8e0621b35c72532347564f056e907b48af
onetrueallen <onetrueallen@users.noreply.github.com>
2017-01-07 14:42:27 -0500

    Fixed import statements.

--- app/src/main/java/com/example/v2/legit/MainActivity.java
+++ app/src/main/java/com/example/v2/legit/MainActivity.java
@@ -1,39 +1,43 @@
-package com.example.v2.legit;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import javax.naming.Context;
+import javax.swing.text.View;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-	
-	private void writeToFile(String data,Context context) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+
+	private void writeToFile(String data, Context context) {
 		try {
-			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
+					context.openFileOutput("config.txt", Context.MODE_PRIVATE));
 			outputStreamWriter.write(data);
 			outputStreamWriter.close();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			Log.e("Exception", "File write failed: " + e.toString());
-		} 
+		}
 	}
-	
+
 	private String readFromFile(Context context) {
 
 		String ret = "";
@@ -41,21 +45,20 @@ public class MainActivity extends AppCompatActivity {
 		try {
 			InputStream inputStream = context.openFileInput("config.txt");
 
-			if ( inputStream != null ) {
+			if (inputStream != null) {
 				InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 				String receiveString = "";
 				StringBuilder stringBuilder = new StringBuilder();
 
-				while ( (receiveString = bufferedReader.readLine()) != null ) {
+				while ((receiveString = bufferedReader.readLine()) != null) {
 					stringBuilder.append(receiveString);
 				}
 
 				inputStream.close();
 				ret = stringBuilder.toString();
 			}
-		}
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			Log.e("login activity", "File not found: " + e.toString());
 		} catch (IOException e) {
 			Log.e("login activity", "Can not read file: " + e.toString());
